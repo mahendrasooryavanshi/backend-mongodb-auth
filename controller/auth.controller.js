e279db9afcdb93dbc9cb45db615c359ce7f8834c
Mahendra Suryawanshi <mahendrasooryavanshi@gmail.com>
2025-11-04 18:32:12 +0530

    recreating all files

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,12 +1,40 @@
-const { sign } = require("jsonwebtoken");
+// const { sign } = require("jsonwebtoken");
 
-module.exports = {
+const { tokenMiddleware } = require("../middleware/token.middleware");
+const { userService } = require("../service/user.service");
+
+module.exports.AuthController = {
     signUp: async (req, res) => {
         try {
-            const data = req.body
-            console.log("data")
-            return res.json({
-                data: data
+            const body = req.body
+            const existingUser = await userService.findOne({
+                $or: [{ eamil: body.eamil }, { mobile: body.mobile }]
+            })
+            if (existingUser) {
+                return res.status(404).json({
+                    status: 400,
+                    message: existingUser.email == body.email ? "Email already exist" : "Mobile number is already exist."
+                })
+            }
+            const user = await userService.create(body)
+            const payload = {
+                id: user.id,
+                email: user.email,
+                role: user.role,
+            }
+            const accessToken = await tokenMiddleware.genrateToken(payload, { type: "access" });
+            const refreshToken = await tokenMiddleware.genrateToken(payload, { type: "refresh" });
+            return res.status(201).json({
+                status: 201,
+                message: "User register successfully",
+                data: {
+                    tokenType: "Bearer",
+                    role: user.role,
+                    username: user.firstName + " " + user.lastName,
+                    accessToken: accessToken,
+                    refeshToken: refreshToken
+                }
+
             })
         } catch (error) {
             console.log("Error signup controller", error);
--- router/auth.router.js
+++ router/auth.router.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { AuthController } = require("../controller/auth.controller");
 const router = express.Router()
-
-// router.post("/signup")
+const { validate } = require("../middleware/validation");
+const { createUserSchema } = require("../validation/auth.validation");
+router.post("/signup", validate(createUserSchema), AuthController.signUp)
 
 module.exports = router
